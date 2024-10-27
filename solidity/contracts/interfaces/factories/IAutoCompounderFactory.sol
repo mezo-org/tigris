45e16d50ad332df73c608678b71c67dacfd6d33a
Matt Luongo <mhluongo@gmail.com>
2024-10-27 16:29:00 -0400

    Add compiler errors and events to IAutoCompounderFactory

--- solidity/contracts/interfaces/factories/IAutoCompounderFactory.sol
+++ solidity/contracts/interfaces/factories/IAutoCompounderFactory.sol
@@ -3,5 +3,16 @@
 pragma solidity 0.8.24;
 
 interface IAutoCompounderFactory {
+
+    event SetRewardAmount(uint256 amount);
+    event CreateAutoCompounder(address created, address admin, address autoCompounder);
+
+    error TokenIdZero();
+    error TokenIdNotApproved();
+    error TokenIdNotManaged();
+    error NotTeam();
+    error AmountSame();
+    error AmountOutOfAcceptableRange();
+
     function rewardAmount() external returns (uint256) {}
 }
