e11a0bfbce07b46998ac47bef122a074667de14f
Antonio Salazar Cardozo <antonio@thesis.co>
2024-12-03 16:32:03 -0500

    Link finance/cathedral from bazaar

--- dapp/app/bazaar/page.tsx
+++ dapp/app/bazaar/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <Providers>
       <DEX />
+      <a id="bazaar" className="fixed rotate-[270deg] left-0 top-1/2 -translate-y-1/2 translate-x-[-25%] bg-slate-300 hover:bg-slate-200 py-1 px-2" href="/">Finance</a>
     </Providers>
   )
 }
