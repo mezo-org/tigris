a055a1bc2118820899f835989f12a124bba077a1
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-02-19 16:56:57 +0100

    Add missing pragma to `Voter`

--- solidity/contracts/Voter.sol
+++ solidity/contracts/Voter.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+pragma solidity 0.8.24;
+
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IVotingRewardsFactory} from "./interfaces/factories/IVotingRewardsFactory.sol";
 import {IGauge} from "./interfaces/IGauge.sol";
