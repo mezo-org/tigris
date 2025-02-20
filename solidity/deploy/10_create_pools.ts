365e978749209dbbc03c5a40cd1f75b4e63a75a6
Piotr Dyraga <piotr.dyraga@thesis.co>
2025-02-20 14:33:53 +0100

    Create pools and gauges for matsnet
    Three pools and gauges are created for matsnet governance:
    - BTC/mUSD
    - mUSD/LIMPETH
    - mUSD/wtBTC
    
    LIMPETH and wtBTC are community tokens.

--- solidity/deploy/10_create_pools.ts
+++ solidity/deploy/10_create_pools.ts
@@ -7,35 +7,73 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { execute, log } = deployments
   const { deployer } = await getNamedAccounts()
 
-  const btcAddress = (await deployments.get("Bitcoin")).address
-  const musdAddress = (await deployments.get("mUSD")).address
-
-  log(`Bitcoin address is ${btcAddress}`)
-  log(`mUSD address is ${musdAddress}`)
-
   const poolFactory = await helpers.contracts.getContract("PoolFactory")
+  const veBTCVoter = await helpers.contracts.getContract("VeBTCVoter")
 
-  const pool = await poolFactory.getPair(btcAddress, musdAddress, false)
+  const votingRewardsFactory = await deployments.get("VotingRewardsFactory")
+  const gaugeFactory = await deployments.get("GaugeFactory")
 
-  if (pool !== ethers.ZeroAddress) {
-    log(`BTC-mUSD pool already deployed at ${pool}`)
-  } else {
-    log("Creating BTC-mUSD pool...")
+  const createPool = async (
+    token1: string,
+    token2: string,
+    isStable: boolean,
+  ) => {
+    const token1Address = (await deployments.get(token1)).address
+    const token2Address = (await deployments.get(token2)).address
 
-    await execute(
-      "PoolFactory",
-      { from: deployer, log: true, waitConfirmations: 1 },
-      "createPair",
-      btcAddress,
-      musdAddress,
-      false,
-    )
+    let pool = await poolFactory.getPair(token1Address, token2Address, isStable)
+    if (pool !== ethers.ZeroAddress) {
+      log(`${token1}/${token2} pool already exists at ${pool}`)
+    } else {
+      log(`Creating ${token1}/${token2} pool...`)
+      await execute(
+        "PoolFactory",
+        { from: deployer, log: true, waitConfirmations: 1 },
+        "createPair",
+        token1Address,
+        token2Address,
+        isStable,
+      )
+
+      pool = await poolFactory.getPair(token1Address, token2Address, isStable)
+      log(`${token1}/${token2} pool created at ${pool}`)
+    }
+
+    let gauge = await veBTCVoter.gauges(pool)
+    if (gauge !== ethers.ZeroAddress) {
+      log(`${token1}/${token2} gauge already exists at ${gauge}`)
+    } else {
+      log(`Creating ${token1}/${token2} gauge...`)
+      await execute(
+        "VeBTCVoter",
+        { from: deployer, log: true, waitConfirmations: 1 },
+        "createGauge",
+        await poolFactory.getAddress(),
+        votingRewardsFactory.address,
+        gaugeFactory.address,
+        pool,
+      )
+
+      gauge = await veBTCVoter.gauges(pool)
+      log(`${token1}/${token2} gauge created at ${gauge}`)
+    }
   }
+
+  await createPool("Bitcoin", "mUSD", false)
+  await createPool("mUSD", "LIMPETH", false)
+  await createPool("mUSD", "wtBTC", false)
 }
 
 export default func
 
 func.tags = ["CreatePools"]
-func.dependencies = ["Bitcoin", "PoolFactory"]
+func.dependencies = [
+  "Bitcoin",
+  "mUSD",
+  "PoolFactory",
+  "GaugeFactory",
+  "VotingRewardsFactory",
+  "VeBTCVoter",
+]
 
 func.skip = async (hre) => hre.network.name !== "matsnet"
