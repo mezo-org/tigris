b44151de10221154dfa97eea214cd481cc39de9a
Dymitr (Dmitry) Paremski <dymitr.paremski@thesis.co>
2025-02-20 13:43:10 +0100

    Pinning to 0.8.24 solidity version

--- solidity/contracts/FeeSplitter.sol
+++ solidity/contracts/FeeSplitter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity 0.8.24;
 
 import {IVoter} from "./interfaces/IVoter.sol";
 import {IEpochGovernor} from "./interfaces/IEpochGovernor.sol";
