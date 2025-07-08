4be2f4c70e4c73c01f5e5b6407f318e4231f3c33
Jakub Nowakowski <jakub.nowakowski@thesis.co>
2025-07-08 13:49:55 +0200

    Update the placeholder script to not fail deployment

--- solidity/deploy/18_deploy_ve_mezo.ts
+++ solidity/deploy/18_deploy_ve_mezo.ts
@@ -1,1 +1,11 @@
-// placeholder
+import { DeployFunction, DeployOptions } from "hardhat-deploy/dist/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // placeholder
+}
+
+export default func
+
+func.tags = ["VeMEZO"]
+func.dependencies = []
