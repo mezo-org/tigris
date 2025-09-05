a2c0195d63b6fe58c57a576997f717c55b9a6045
Rafal Czajkowski <rafal.czajkowski@thesis.co>
2025-09-05 10:42:19 +0200

    Expose `/metrics` endpoint for Prometheus
    We want to be notified if something goes wrong so that we can take
    action manually, so we need to integrate with our monitoring system.
    Exposing the timestamp of the last successful transaction
    (`updatePeriod`) allows us to detect potential issues. If no transaction
    has been completed within the past week, it may indicate a problem with
    maintainer that requires our attention.

--- infrastructure/cloudflare/fee-splitter/src/index.ts
+++ infrastructure/cloudflare/fee-splitter/src/index.ts
@@ -1,7 +1,28 @@
 import splitRewards from "./split-rewards"
 import { Env } from "./types"
+import updatePeriodTracker from "./update-period-tracker"
 
 export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url)
+
+    if (url.pathname === "/metrics") {
+      const timestamp = updatePeriodTracker.getLastSuccessfulTransaction(env)
+
+      const metrics =
+        "# HELP last_update_period Timestamp of the last successfully completed transaction.\n" +
+        "# TYPE last_update_period gauge\n" +
+        `last_update_period ${timestamp}`
+
+      return new Response(metrics, {
+        headers: {
+          "Content-Type": "text/plain; version=0.0.4; charset=utf-8",
+        },
+      })
+    }
+
+    return new Response("OK")
+  },
   async scheduled(controller: { cron: unknown }, env: Env) {
     try {
       switch (controller.cron) {
